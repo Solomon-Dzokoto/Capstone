d48a5289899dddadb5d0ae8c1daa607d03ffc43c
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-03-14 16:21:41 +0000

    feat: add svg files

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,5 +1,9 @@
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <aside className="w-[14.5rem] ">
+      <img src="/assets/Logo (1).png" alt="Logo" />
+    </aside>
+  );
 };
 
 export default Sidebar;
